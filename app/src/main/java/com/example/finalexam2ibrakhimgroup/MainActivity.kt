49d71c89bea041075732ea9fc4e90d3ec99eff7b
Timur2010-creator <timuraitbaev7@gmail.com>
2025-11-27 01:19:34 +0600

    ++

--- app/src/main/java/com/example/finalexam2ibrakhimgroup/MainActivity.kt
+++ app/src/main/java/com/example/finalexam2ibrakhimgroup/MainActivity.kt
@@ -9,7 +9,7 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
     private lateinit var pref: PrefManager
-    private var counter = 0
+    private var counter = 0;
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
