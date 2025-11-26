60e773c5c49f0fcd35eab62c0e4f13c09e4e2bad
Timur2010-creator <timuraitbaev7@gmail.com>
2025-11-27 01:12:17 +0600

    +++

--- app/src/main/java/com/example/finalexam2ibrakhimgroup/MainActivity.kt
+++ app/src/main/java/com/example/finalexam2ibrakhimgroup/MainActivity.kt
@@ -1,30 +1,55 @@
 package com.example.finalexam2ibrakhimgroup
 
 import android.os.Bundle
-import android.widget.Button
-import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import com.example.finalexam2ibrakhimgroup.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    private var counter = 0
+
+    private lateinit var binding: ActivityMainBinding
     private lateinit var pref: PrefManager
+    private var counter = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        pref= PrefManager(this)
 
-        val counterText = findViewById<TextView>(R.id.tv_Text)
-        val incrementButton = findViewById<Button>(R.id.Button)
+        // Инициализация ViewBinding
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        pref = PrefManager(this)
+
         counter = pref.getCount()
-        counterText.text = counter.toString()
+        binding.tvText.text = counter.toString()
 
+        updateButtonText(counter)
 
-        incrementButton.setOnClickListener {
-            counter++
-            counterText.text = counter.toString()
+        binding.Button.setOnClickListener {
+            if (binding.Button.text == "+") {
+                if (counter < 10) {
+                    counter++
+                }
+            } else {  "-"
+                if (counter > 0) {
+                    counter--
+                }
+            }
+
+            binding.tvText.text = counter.toString()
+            updateButtonText(counter)
+
+            // Сохраняем значение
             pref.saveCount(counter)
+        }
+    }
+
+    private fun updateButtonText(counter: Int) {
+        if (counter == 10) {
+            binding.Button.text = "-"
+        } else if (counter == 0) {
+            binding.Button.text = "+"
         }
     }
 }
